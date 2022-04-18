4ec5bce5a129ccbf52832e26d5f89abd9d9f54ea
うづき <uzuky.04@gmail.com>
2022-04-18 21:32:54 +0900

    令和最新版

--- open.js
+++ open.js
@@ -1,15 +1,4 @@
 function allopen(){
-	window.open("http://shootingstar.fm/toshi_a.html","shootingstar.fm");
-	window.open("http://shootingstar067.com/toshi_a.html","shootingstar067.com");
-	window.open("http://epsilonman.net/toshi_a.html","epsilonman.net");
-	window.open("http://uniunichan.web.fc2.com/toshi_a.html","uniunichan.web.fc2.com");
-	window.open("http://teokurebsd.org/toshi_a.html","teokurebsd.org");
-	window.open("http://rinchan.jp/toshi_a.html","rinchan.jp");
-	window.open("http://detteiu.com/toshi_a.html","detteiu.com");
-	window.open("http://peccu.sytes.net/toshi_a.html","peccu.sytes.net");
-	window.open("https://rhe.jp/toshi_a.xhtml","rhe.jp");
 	window.open("http://d250g2.com/toshi_a.html","d250g2.com");
-	window.open("http://haruicon.com/toshi_a.html","haruicon.com");
-	window.open("http://opencocon.org/toshi_a/","opencocon.org");
-	
+	window.open("https://karno.dev/toshi_a.html","karno.dev");
 }
